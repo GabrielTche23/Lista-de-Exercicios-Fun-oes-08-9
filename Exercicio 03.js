d8ceee982cb7f1cfb634a8ad58db95efa9250211
Gabriel Rodrigo <pdktche-pb@live.com>
2022-09-08 22:48:36 -0300

    Foi criado as funçoes de caulcular a media do salario e dos filhos.

---
+++ Exercicio 03.js
@@ -0,0 +1,24 @@
+let arraySalario = [630,2680,1600,1214]
+let arrayFilhos = [6,3,4,1]
+
+//let perguntaSalario = prompt("Digite o seu salario?")
+//let perguntaFilhos = prompt("Digite o numero de filhos que você possue: ")
+console.log(CalcularMediaSalario())
+console.log(CalcularMediaFilhos())
+
+function CalcularMediaSalario(){
+    let mediaSalario = 0
+    for (let contador = 0; contador < arraySalario.length; contador++){
+        mediaSalario = mediaSalario + arraySalario[contador]
+    }
+    return (mediaSalario / arraySalario.length)
+}
+
+function CalcularMediaFilhos(){
+    let mediaFilhos = 0
+    for (let contador = 0; contador < arrayFilhos.length; contador++){
+        mediaFilhos = mediaFilhos + arrayFilhos[contador]
+    }
+    return mediaFilhos / arrayFilhos.length
+}
+
